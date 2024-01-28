7d30852ea02507c661f25bd2b90a9f38e486fd41
ahmedakef <aemed.akef.1@gmail.com>
2024-01-28 10:51:14 +0100

    handle starting with function definition

--- manager.go
+++ manager.go
@@ -14,12 +14,12 @@ const (
 )
 
 type Manager struct {
-	commands            []string
-	functions           []string
-	variables           map[string]struct{}
-	lastCommandFunction bool
-	programPath         string
-	templatePath        string
+	commands             []string
+	functions            []string
+	variables            map[string]struct{}
+	lastInputFunctionDef bool
+	programPath          string
+	templatePath         string
 }
 
 func newManager() *Manager {
@@ -47,15 +47,15 @@ func (m *Manager) addCommand(command string) {
 	}
 	m.commands = append(m.commands, command)
 
-	m.lastCommandFunction = false
+	m.lastInputFunctionDef = false
 }
 func (m *Manager) addFunction(function string) {
 	m.functions = append(m.functions, function)
-	m.lastCommandFunction = true
+	m.lastInputFunctionDef = true
 }
 
 func (m *Manager) removeLastInput() {
-	if m.lastCommandFunction {
+	if m.lastInputFunctionDef {
 		m.functions = m.functions[:len(m.functions)-1]
 	} else {
 		lastElementIndex := len(m.commands) - 1
@@ -69,14 +69,17 @@ func (m *Manager) removeLastInput() {
 
 func (m *Manager) runProgram() error {
 	commands := make([]string, len(m.commands)+1)
-	lastElementIndex := len(m.commands) - 1
 	copy(commands, m.commands) // copy to avoid modifying the original slice
-	if isExperimentalInput(m.commands[lastElementIndex]) {
-		commands[lastElementIndex] = commandPrintted(m.commands[lastElementIndex])
-		m.commands = m.commands[:lastElementIndex]
+	if !m.lastInputFunctionDef {
+		lastElementIndex := len(m.commands) - 1
+		if isExperimentalInput(m.commands[lastElementIndex]) {
+			commands[lastElementIndex] = commandPrintted(m.commands[lastElementIndex])
+			m.commands = m.commands[:lastElementIndex]
+		}
 	}
-	commands = append(commands, m.useCall())
-
+	if len(m.variables) > 0 {
+		commands = append(commands, m.useCall())
+	}
 	program, err := prepareProgram(m.templatePath, commands, m.functions)
 	if err != nil {
 		fmt.Println("Error:", err)
